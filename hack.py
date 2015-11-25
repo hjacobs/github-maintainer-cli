4da7b41c8dd7ce84367b5755b9fd3af898481184
hjacobs <henning@jacobs1.de>
2015-11-25 23:28:42 +0100

    allow listing issues/PRs

--- hack.py
+++ hack.py
@@ -2,12 +2,14 @@
 
 import click
 import codecs
+import datetime
 import os
 import requests
 import stups_cli.config
+import time
 import yaml
 
-from clickclick import print_table, Action
+from clickclick import print_table, Action, AliasedGroup
 
 CONFIG_DIR = click.get_app_dir('github-maintainer-cli')
 
@@ -15,6 +17,21 @@ adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10)
 session = requests.Session()
 session.mount('http://', adapter)
 session.mount('https://', adapter)
+
+
+def parse_time(s: str) -> float:
+    '''
+    >>> parse_time('2015-04-14T19:09:01Z') > 0
+    True
+    '''
+    try:
+        utc = datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%SZ')
+        ts = time.time()
+        utc_offset = datetime.datetime.fromtimestamp(ts) - datetime.datetime.utcfromtimestamp(ts)
+        local = utc + utc_offset
+        return local.timestamp()
+    except:
+        return None
 
 
 def get_my_issues(token):
@@ -61,8 +78,25 @@ def get_my_repos(my_emails, token):
             break
 
 
-@click.command()
+def get_repositories():
+    path = os.path.join(CONFIG_DIR, 'repositories.yaml')
+
+    try:
+        with open(path) as fd:
+            repositories = yaml.safe_load(fd)
+    except:
+        repositories = {}
+    return repositories
+
+
+@click.group(cls=AliasedGroup)
 def cli():
+    pass
+
+
+@cli.command()
+def repositories():
+    '''List repositories'''
     config = stups_cli.config.load_config('github-maintainer-cli')
     emails = config.get('emails')
     token = config.get('github_access_token')
@@ -73,15 +107,9 @@ def cli():
     if not token:
         raise click.UsageError('No GitHub access token configured')
 
-    os.makedirs(CONFIG_DIR, exist_ok=True)
-
     path = os.path.join(CONFIG_DIR, 'repositories.yaml')
 
-    try:
-        with open(path) as fd:
-            repositories = yaml.safe_load(fd)
-    except:
-        repositories = {}
+    repositories = get_repositories()
 
     if not repositories:
         with Action('Scanning repositories..') as act:
@@ -89,6 +117,7 @@ def cli():
                 repositories[repo['url']] = repo
                 act.progress()
 
+        os.makedirs(CONFIG_DIR, exist_ok=True)
         with open(path, 'w') as fd:
             yaml.safe_dump(repositories, fd)
 
@@ -105,6 +134,56 @@ def cli():
 
     print_table(['full_name', 'stargazers_count', 'forks_count', 'open_issues', 'open_pull_requests'], rows)
 
+
+@cli.command()
+def issues():
+    '''List open issues'''
+    config = stups_cli.config.load_config('github-maintainer-cli')
+    token = config.get('github_access_token')
+
+    repositories = get_repositories()
+
+    rows = []
+    for issue in get_my_issues(token):
+        if not issue.get('pull_request'):
+            repo = repositories.get(issue['repository']['url'])
+            if repo:
+                issue['repository'] = repo['full_name']
+                issue['created_time'] = parse_time(issue['created_at'])
+                issue['created_by'] = issue['user']['login']
+                issue['labels'] = ', '.join([l['name'] for l in issue['labels']])
+                rows.append(issue)
+
+    rows.sort(key=lambda x: (x['repository'], x['number']))
+    print_table(['repository', 'number', 'title', 'labels', 'created_time', 'created_by'], rows)
+
+
+@cli.command('pull-requests')
+def pull_requests():
+    '''List pull requests'''
+    config = stups_cli.config.load_config('github-maintainer-cli')
+    token = config.get('github_access_token')
+
+    repositories = get_repositories()
+
+    rows = []
+    for issue in get_my_issues(token):
+        pr = issue.get('pull_request')
+        if pr:
+            repo = repositories.get(issue['repository']['url'])
+            if repo:
+                r = session.get(pr['url'], headers={'Authorization': 'Bearer {}'.format(token)})
+                pr = r.json()
+                issue.update(**pr)
+                issue['repository'] = repo['full_name']
+                issue['created_time'] = parse_time(issue['created_at'])
+                issue['created_by'] = issue['user']['login']
+                issue['labels'] = ', '.join([l['name'] for l in issue['labels']])
+                rows.append(issue)
+
+    rows.sort(key=lambda x: (x['repository'], x['number']))
+    print_table(['repository', 'number', 'title', 'labels', 'mergeable',
+                 'mergeable_state', 'created_time', 'created_by'], rows)
 
 if __name__ == '__main__':
     cli()
